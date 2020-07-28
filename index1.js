096acb4bd93369c135a9f7c547ef7656cd48489c
shiwani472 <67495985+shiwani472@users.noreply.github.com>
2020-07-28 00:07:25 -0600

    Delete index1.js

--- index1.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Student from './student';
-import App from './App';
-ReactDOM.render(<Student/>,document.getElementById("root"));
-
